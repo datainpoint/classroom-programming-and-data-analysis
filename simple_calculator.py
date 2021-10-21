28c603f4220cd97de6fb2b20bb3751295ff2dc22
yaojenkuo <tonykuoyj@gmail.com>
2021-10-21 14:56:40 +0800

    update

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,16 +1,16 @@
 # simple_calculator.py
 class SimpleCalculator:
     """
-    This class creates a simple calculator that is able to add, subtract, multiply, and divide 2 numbers.
+    This class creates a simple calculator that is able to add and subtract 2 numbers.
     This class has an attribute of Euler's number: e.
     """
     def __init__(self):
         self.e = 2.71828182846
+    def __repr__(self):
+        return "A Simple Calculator Class."
     def add(self, x, y):
         return x + y
     def subtract(self, x, y):
         return x - y
-    def multiply(self, x, y):
-        return x * y
-    def divide(self, x, y):
-        return x / y
+    def exp(self, n):
+        return self.e**n
